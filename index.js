82651402850e8652e25fefe652ae6e4c1e0a0399
Carl Corsini <carlcorsin@yahoo.com>
2020-05-12 19:19:21 -0700

    loop

--- index.js
+++ index.js
@@ -4,20 +4,28 @@ window.onload = () => {
   // grab container with javascript
   let container = document.querySelector('#container')
   // create div element with .createElement
-  let div = document.createElement('div')
-  // add class of sqr to created element
-  div.classList.add('sqr')
-  // use .appendChild to add div to container
-  container.appendChild(div)
-  // add event listener to div
-  div.addEventListener('click', function(){
-    // change div backgroundColor to black
-    div.style.backgroundColor = currentColor
-
-
-
-  })
   
+  // created wacky loop
+  for (let i = 0; i < 100; i++) {
+
+    let div = document.createElement('div')
+    // add class of sqr to created element
+    div.classList.add('sqr')
+    // use .appendChild to add div to container
+    container.appendChild(div)
+    // add event listener to div
+    div.addEventListener('click', function(){
+      // change div backgroundColor to black
+      div.style.backgroundColor = currentColor
+  
+  
+  
+    })
+  }
+
+
+
+
   let paletteColor = 'coral'
   // select canvas with javaScript
   let palette = document.querySelector('#palette')
